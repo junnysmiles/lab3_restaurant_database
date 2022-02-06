a79b077870cef2bafd5a690108b9dff98610e18e
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2022-02-06 16:09:26 -0500

    Added models and routes folders, created index.js and restaurant router

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const restaurantRouter = require('./routes/RestaurantRoutes.js')
+
+const app = express()
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://junnysmiles:junny1234@fullstack.xy5fk.mongodb.net/gbc-winter2022?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(success => {
+    console.log('Mongodb connection successful!')
+}).catch(err => {
+    console.log('MongoDB Connection ERROR...')
+})
+
+app.use(restaurantRouter)
+
+app.listen(8080, () => { console.log('Server is running...') })
